6324ca71f4d6fd465a163d9f5943292550563c50
Matthew Shen <78379726+MatthewDShen@users.noreply.github.com>
2023-04-25 16:43:32 -0400

    setup page

---
+++ rat-city.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.markdown("# Title")
+
+#############################################
+
+st.markdown("### Title")
+
+#############################################
+
+st.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac ultricies lacus. Proin gravida magna magna, quis consequat est ultricies sed. Sed quis lorem libero. Morbi vel sapien nibh.")
+
+st.markdown("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac ultricies lacus. Proin gravida magna magna, quis consequat est ultricies sed. Sed quis lorem libero. Morbi vel sapien nibh. Cras aliquet tempor dui ornare volutpat. Maecenas quam est, eleifend sit amet varius eu, semper nec urna. Aenean eleifend, magna sit amet vestibulum hendrerit, lectus leo tempus dui, in dictum leo mi in neque. Maecenas cursus nec purus eu pellentesque. Sed a augue aliquam, dictum urna nec, aliquam sapien.
+
+Pellentesque porta elit ante, eget sagittis sem sagittis vel. Cras venenatis et sem nec suscipit. Nulla diam justo, dignissim non risus ut, aliquam venenatis est. Vivamus facilisis ac dolor et mollis. Nullam sodales, nisl nec interdum hendrerit, turpis orci dignissim erat, eget faucibus lorem nisl sit amet diam. Suspendisse lacinia id justo eget pellentesque. Suspendisse ut facilisis ligula. Duis sapien magna, lobortis nec volutpat in, commodo sed augue. Etiam augue lectus, lobortis eget dapibus ac, malesuada at felis. In lacinia dui sit amet odio volutpat euismod. Aliquam ornare nulla eu ligula eleifend, vitae euismod ante faucibus. Sed quis leo in sem pulvinar condimentum eget non eros.
+""")
