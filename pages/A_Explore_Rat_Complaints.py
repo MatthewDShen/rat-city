03d578abef98d094c7f54ad0e6af93464c31fd7a
Matthew Shen <78379726+MatthewDShen@users.noreply.github.com>
2023-04-25 19:39:48 -0400

    remove api loading information

--- pages/A_Explore_Rat_Complaints.py
+++ pages/A_Explore_Rat_Complaints.py
@@ -18,50 +18,6 @@ st.markdown("### Below you can find the rat complaints in NYC over the past mont
 
 #############################################
 
-# Insert Functions Here
-def load_data(endpoint):
-    '''
-    Loads in data from NYC Open Data
-
-    Input:
-        - Endpoint (example: "a0aa-aaaa")
-        - Query in SQL format
-
-    Output:
-        - Dataframe with information from client
-    '''
-    # Set up endpoint
-    endpoint = endpoint
-
-    # Load api key
-    load_dotenv()
-
-    # Get client information
-    client = Socrata('data.cityofnewyork.us',
-                    os.getenv('ny_app_token'),
-                    username = os.getenv('ny_api_key_id'),
-                    password = os.getenv('ny_api_key_secret'))
-    
-    # Get total number of records in api
-    # query_count = "SELECT COUNT(*)"
-    # NUM_RECORDS = int(client.get(endpoint, query = query_count)[0]['COUNT'])
-    NUM_RECORDS = 2000
-    
-    query = f"""
-        SELECT *
-        LIMIT {NUM_RECORDS}
-    """
-    
-    # Get results from client
-    results = client.get(endpoint, query=query)
-
-    # Change results into dataframe
-    df = pd.DataFrame.from_records(results)
-
-    return df
-
-print(load_data('cvf2-zn8s'))
-
 def load_rat_data_drive():
     url = 'https://drive.google.com/file/d/11aUJLdJqLDfVq_LAbyw3DDKxZYFbEZH0/view?usp=share_link'
     url = 'https://drive.google.com/uc?id=' + url.split('/')[-2]
@@ -88,7 +44,9 @@ def convert_df(df):
 
 df = load_data('cvf2-zn8s')
 
+print(df)
+
 if df is not None:
     # Front End UI
-    st.write(df)
+    st.write(df.head())
     
