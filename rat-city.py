7a8d4976dc14e1f53087277b8b3ad8189d6adad3
mds377 <mds377@cornell.edu>
2023-05-01 14:19:17 -0400

    Add split between scratch and existing zip code. Created population selector

--- rat-city.py
+++ rat-city.py
@@ -1,18 +1,48 @@
+import pandas as pd
+import pickle
 import streamlit as st
 
 
-st.markdown("# Rat City")
+
+st.markdown('# Rat City')
 
 #############################################
 
-st.markdown("### Title")
+st.markdown('### The objective of this project is to let people see how changes in restaurants effect the number of rat sightings in an area')
+st.markdown('### This program is currently only available for New York City')
 
 #############################################
 
-st.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac ultricies lacus. Proin gravida magna magna, quis consequat est ultricies sed. Sed quis lorem libero. Morbi vel sapien nibh.")
 
-st.markdown("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac ultricies lacus. Proin gravida magna magna, quis consequat est ultricies sed. Sed quis lorem libero. Morbi vel sapien nibh. Cras aliquet tempor dui ornare volutpat. Maecenas quam est, eleifend sit amet varius eu, semper nec urna. Aenean eleifend, magna sit amet vestibulum hendrerit, lectus leo tempus dui, in dictum leo mi in neque. Maecenas cursus nec purus eu pellentesque. Sed a augue aliquam, dictum urna nec, aliquam sapien.
+# TODO: Import ML model with pickle
 
-Pellentesque porta elit ante, eget sagittis sem sagittis vel. Cras venenatis et sem nec suscipit. Nulla diam justo, dignissim non risus ut, aliquam venenatis est. Vivamus facilisis ac dolor et mollis. Nullam sodales, nisl nec interdum hendrerit, turpis orci dignissim erat, eget faucibus lorem nisl sit amet diam. Suspendisse lacinia id justo eget pellentesque. Suspendisse ut facilisis ligula. Duis sapien magna, lobortis nec volutpat in, commodo sed augue. Etiam augue lectus, lobortis eget dapibus ac, malesuada at felis. In lacinia dui sit amet odio volutpat euismod. Aliquam ornare nulla eu ligula eleifend, vitae euismod ante faucibus. Sed quis leo in sem pulvinar condimentum eget non eros.
-""")
+# TODO: Import model data
+data_df = pd.DataFrame({'zip code number': [10001,12001], 'population estimate': [1000,5000]})# test df
 
+
+st.markdown('#### Select how you would like to build your neighborhood')
+## Pick starting zipcode ##
+col1, col2 = st.columns(2)
+scratch_bool = True
+### Starting from a blank neighborhood
+with(col1):
+    if st.button('Create Your Own Neighborhood from Scratch'):
+        scratch_bool = True        
+### Base your neighborhood from an existing zipcode        
+with(col2):
+    zip_int = st.number_input('Adjust Existing Zipcode', min_value = min(data_df['zip code number']), max_value = max(data_df['zip code number']))
+    zip_row_int = data_df[data_df['zip code number'] == zip_int].index[0]
+    
+    if (zip_int and st.button('Confirm Zipcode')):
+        scratch_bool = False
+
+# Show current state of zipcode
+if scratch_bool == False:
+    st.write('Zipcode {} Currently Information'.format(zip_int))
+    st.write('Population: {}'.format(data_df.iloc[zip_row_int]['population estimate']))
+
+# Population
+if scratch_bool:
+    st.number_input('Select Population', min_value = 0)
+else:
+    st.number_input('Select Population', min_value = 0, value = data_df.iloc[zip_row_int]['population estimate'])
