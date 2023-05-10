51cabe8dbec3c2848a8a3f8ed567abe32ffca9e3
Matthew Shen <78379726+MatthewDShen@users.noreply.github.com>
2023-05-10 13:51:30 -0400

    Streamlit (#6)
    * Add split between scratch and existing zip code. Created population selector
    
    * add comments about model
    
    * fix problems with the package requirements
    
    * reorg folders

--- rat-city.py
+++ rat-city.py
@@ -1,18 +1,23 @@
+import pandas as pd
+import pickle
 import streamlit as st
 
 
-st.markdown("# Rat City")
+
+st.markdown('# Rat City')
 
 #############################################
 
-st.markdown("### Title")
+st.markdown('### This website lets people see how restaurants effect the number of rat sightings in an area')
+st.markdown('This program is currently only available for New York City')
 
 #############################################
 
-st.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac ultricies lacus. Proin gravida magna magna, quis consequat est ultricies sed. Sed quis lorem libero. Morbi vel sapien nibh.")
+st.markdown('### On this page you can explore the data that we used to predict the number of rat sightings')
 
-st.markdown("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac ultricies lacus. Proin gravida magna magna, quis consequat est ultricies sed. Sed quis lorem libero. Morbi vel sapien nibh. Cras aliquet tempor dui ornare volutpat. Maecenas quam est, eleifend sit amet varius eu, semper nec urna. Aenean eleifend, magna sit amet vestibulum hendrerit, lectus leo tempus dui, in dictum leo mi in neque. Maecenas cursus nec purus eu pellentesque. Sed a augue aliquam, dictum urna nec, aliquam sapien.
+data_df = pd.DataFrame({'zip code number': [10001,12001], # test df
+                        'population estimate': [1000,5000],
+                        'Number of Resaurants': [50,100],
+                       })
 
-Pellentesque porta elit ante, eget sagittis sem sagittis vel. Cras venenatis et sem nec suscipit. Nulla diam justo, dignissim non risus ut, aliquam venenatis est. Vivamus facilisis ac dolor et mollis. Nullam sodales, nisl nec interdum hendrerit, turpis orci dignissim erat, eget faucibus lorem nisl sit amet diam. Suspendisse lacinia id justo eget pellentesque. Suspendisse ut facilisis ligula. Duis sapien magna, lobortis nec volutpat in, commodo sed augue. Etiam augue lectus, lobortis eget dapibus ac, malesuada at felis. In lacinia dui sit amet odio volutpat euismod. Aliquam ornare nulla eu ligula eleifend, vitae euismod ante faucibus. Sed quis leo in sem pulvinar condimentum eget non eros.
-""")
-
+st.selectbox()
