240b6170fb311fb034146dab9dab0e2e320820a3
Matthew Shen <78379726+MatthewDShen@users.noreply.github.com>
2023-05-11 17:48:38 -0400

    fix feature data duplicates

--- pages/Create Your Own Neighborhood.py
+++ pages/Create Your Own Neighborhood.py
@@ -1,3 +1,4 @@
+import geopandas as gpd
 import pandas as pd
 import pickle
 import streamlit as st
@@ -8,8 +9,7 @@ st.markdown('# Rat City')
 
 #############################################
 
-st.markdown('### The objective of this project is to let people see how changes in restaurants effect the number of rat sightings in an area')
-st.markdown('### This program is currently only available for New York City')
+st.markdown('On this page you can build your own zipcode and see how many rats are predicited to show up')
 
 #############################################
 
@@ -17,63 +17,56 @@ st.markdown('### This program is currently only available for New York City')
 # TODO: Import ML model with pickle
 
 # TODO: Import model data
-data_df = pd.DataFrame({'zip code number': [10001,12001], # test df
-                        'population estimate': [1000,5000],
-                        'Number of Resaurants': [50,100],
-                       })
+df = pd.read_csv('data/processed_data/feature_data.csv')
 
 
-st.markdown('#### Select how you would like to build your neighborhood')
-## Pick starting zipcode ##
-col1, col2 = st.columns(2)
-scratch_bool = True
-### Starting from a blank neighborhood
-with(col1):
-    if st.button('Create Your Own Neighborhood from Scratch'):
-        scratch_bool = True
-        st.write('You have selected to create your own neighborhood')
+st.write(df)
+
+if df is not None:
+    st.markdown('#### Select how you would like to build your neighborhood')
+
+    ##### Pick starting zipcode #####
+    col1, col2 = st.columns(2)
+    scratch_bool = True
+    ### Starting from a blank neighborhood
+    with(col1):
+        if st.button('Create Your Own Neighborhood from Scratch'):
+            scratch_bool = True
+            st.write('You have selected to create your own neighborhood')
+            
+    ### Base your neighborhood from an existing zipcode        
+    with(col2):
+        zip_int = st.selectbox('Select Existing Zipcode', df['zipcode'])
+        zip_index_int = df[df['zipcode'] == zip_int].index[0]
         
-### Base your neighborhood from an existing zipcode        
-with(col2):
-    zip_int = st.number_input('Adjust Existing Zipcode', min_value = min(data_df['zip code number']), max_value = max(data_df['zip code number']))
-    zip_index_int = data_df[data_df['zip code number'] == zip_int].index[0]
-    
-    if (zip_int and st.button('Confirm Zipcode')):
-        scratch_bool = False
-        st.write('You have selected to adjust {}'.format(zip_int))
+        if (zip_int and st.button('Confirm Zipcode')):
+            scratch_bool = False
+            st.write('You have selected to adjust {}'.format(zip_int))
 
-# Show current state of zipcode
-if scratch_bool == False:
-    st.write('Zipcode {} Currently Information'.format(zip_int))
-    st.write('Population: {}'.format(data_df.iloc[zip_index_int]['population estimate']))
+    # Show current state of zipcode
+    if scratch_bool == False:
+        st.write('Zipcode {} Currently Information'.format(zip_int))
+        st.write('Population: {}'.format(df.iloc[zip_index_int]['population estimate']))
 
-# Population
-if scratch_bool:
-    st.number_input('Select Population', min_value = 0)
-else:
-    st.number_input('Select Population', min_value = 0, value = data_df.iloc[zip_index_int]['population estimate'])
-
-col1, col2 = st.columns(2)
-
-with(col1):
-    # Number of Restaurants
+    # Population
     if scratch_bool:
-        num_rest_int = st.number_input('Select Number of Restaurants', min_value = 0)
+        st.number_input('Select Population', min_value = 0)
     else:
-        num_rest_int = st.number_input('Select Number of Restaurants', min_value = 0, value = data_df.iloc[zip_index_int]['Number of Resaurants'])
-        
-with(col2):
-    # Average restaurant rating
-    if scratch_bool:
-        st.number_input('Select the Average Restaurant Score')
+        st.number_input('Select Population', min_value = 0, value = df.iloc[zip_index_int]['population estimate'])
+
+    # col1, col2 = st.columns(2)
+
+    # with(col1):
+    #     # Number of Restaurants
+    #     if scratch_bool:
+    #         num_rest_int = st.number_input('Select Number of Restaurants', min_value = 0)
+    #     else:
+    #         num_rest_int = st.number_input('Select Number of Restaurants', min_value = 0, value = df.iloc[zip_index_int]['Number of Resaurants'])
+            
+    # with(col2):
+    #     # Average restaurant rating
+    #     if scratch_bool:
+    #         st.number_input('Select the Average Restaurant Score')
     
 
     
-#Total inputs
-# Restaurant grade
-# Cuisine
-# Sidewalk Seating
-# Roadway Seating
-# Qualify Alcohol
-# Sidewalk Area
-# Roadway Area
--- rat-city.py
+++ rat-city.py
@@ -1,7 +1,6 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import pandas as pd
-import pickle
 import plotly.express as px
 import streamlit as st
 
@@ -18,20 +17,25 @@ st.markdown('### On this page you can explore the data that we used to predict t
 
 df = pd.read_csv('data/processed_data/feature_data.csv')
 
-st.write(df)
+if df is not None:
+    # st.write(df)
+    st.markdown('#### Explore Features')
 
-st.markdown('#### Explore Relationship with Rat Counts')
-# Histogram
-explore_feature_str = st.selectbox('What feature would you like to explore?', df.columns)
-st.write(px.histogram(data_frame=df, x=explore_feature_str))
+    # Select Feature
+    explore_feature_lst = ['zipcode', 'population', 'area', 'rat count', 'score', 'critical flag', 'action', 'avg score', 'avg flags', 'postcode', 'sidewalk dimensions (area)', 'roadway dimensions (area)', 'approved for sidewalk seating', 'approved for roadway seating', 'qualify alcohol', 'total_number_restaurants']
+    explore_feature_str = st.selectbox('What feature would you like to explore?', explore_feature_lst)
 
-# Map
-df['geometry'] = gpd.GeoSeries.from_wkt(df['geometry'])
-fig, ax = plt.subplots()
-gdf = gpd.GeoDataFrame(df, geometry='geometry', crs='EPSG:2263')
+    # Histogram
+    st.write(px.histogram(data_frame=df, x=explore_feature_str))
 
-gdf.plot(column=explore_feature_str, ax=ax, legend=True)
-plt.title('{} of New York City'.format(explore_feature_str))
-ax.axis('off')
+    # Map
+    df['geometry'] = gpd.GeoSeries.from_wkt(df['geometry'])
+    fig, ax = plt.subplots()
+    gdf = gpd.GeoDataFrame(df, geometry='geometry', crs='EPSG:2263')
+    gdf.plot(column=explore_feature_str, ax=ax, legend=True)
+    plt.title('New York City {}'.format(explore_feature_str))
+    ax.axis('off')
+    st.pyplot(fig)
 
-st.pyplot(fig)
+    # Store Dataframe
+    st.session_state['df'] = df
