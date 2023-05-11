ff999bf64d9eea65dbecb86b552b7e0a344dda1b
Matthew Shen <78379726+MatthewDShen@users.noreply.github.com>
2023-05-11 16:03:25 -0400

    add map plot (#7)

--- rat-city.py
+++ rat-city.py
@@ -1,8 +1,9 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
+import plotly.express as px
 import streamlit as st
-
-
 
 st.markdown('# Rat City')
 
@@ -15,9 +16,20 @@ st.markdown('This program is currently only available for New York City')
 
 st.markdown('### On this page you can explore the data that we used to predict the number of rat sightings')
 
-data_df = pd.DataFrame({'zip code number': [10001,12001], # test df
-                        'population estimate': [1000,5000],
-                        'Number of Resaurants': [50,100],
-                       })
+df = pd.read_csv('processed_data/feature_data.csv')
 
-st.selectbox()
+st.markdown('#### Explore Relationship with Rat Counts')
+# Histogram
+explore_feature_str = st.selectbox('What feature would you like to explore?', df.columns)
+st.write(px.histogram(data_frame=df, x=explore_feature_str))
+
+# Map
+df['geometry'] = gpd.GeoSeries.from_wkt(df['geometry'])
+fig, ax = plt.subplots()
+gdf = gpd.GeoDataFrame(df, geometry='geometry', crs='EPSG:2263')
+
+gdf.plot(column=explore_feature_str, ax=ax, legend=True)
+plt.title('{} of New York City'.format(explore_feature_str))
+ax.axis('off')
+
+st.pyplot(fig)
