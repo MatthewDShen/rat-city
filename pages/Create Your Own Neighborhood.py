d3d0cbee3a218786dc2afdcd12d3b297c7faf4e8
Matthew Shen <78379726+MatthewDShen@users.noreply.github.com>
2023-05-12 14:19:39 -0400

    finish deployment

--- pages/Create Your Own Neighborhood.py
+++ pages/Create Your Own Neighborhood.py
@@ -17,14 +17,12 @@ st.markdown('On this page you can build your own zipcode and see how many rats a
 
 # TODO: Import ML model with pickle
 
-def deploy_model(dict):
+def deploy_model(model,dict):
     rat_count=None
-    model=st.session_state['deploy_model']
 
-    # Add code here
-    rat_count = model.predict(dict)
+    rat_count = model.predict(pd.DataFrame(dict))
 
-    return rat_count
+    return round(rat_count[0])
 
 def show_map(df,zip_index_int):
 
@@ -44,7 +42,10 @@ def show_map(df,zip_index_int):
 df = pd.read_csv('data/processed_data/feature_data.csv')
 df['geometry'] = gpd.GeoSeries.from_wkt(df['geometry'])
 
-if df is not None:
+# Import model
+model = pickle.load(open('model_training/trained_model.pickle', 'rb'))
+
+if df is not None and model is not None:
     st.markdown('#### Select how you would like to build your neighborhood')
 
     ##### Pick starting zipcode #####
@@ -63,7 +64,7 @@ if df is not None:
         if (zip_int and st.button('Confirm Zipcode')):
             st.session_state['scratch_bool'] = False
 
-    user_inputs = {}
+    user_inputs = pd.DataFrame()
             
     if 'scratch_bool' in st.session_state:
         # Confirm Zipcode
@@ -79,34 +80,10 @@ if df is not None:
         # Population
         if st.session_state['scratch_bool']:
             pop_user_select = st.number_input('Select Population', min_value = 0)
-            user_inputs['population'] = pop_user_select
+            user_inputs['population'] = [pop_user_select]
         else:
             pop_user_select = st.number_input('Select Population', min_value = 0, value = int(df.iloc[zip_index_int]['population']))
-            user_inputs['population'] = pop_user_select
-
-        # Area
-        if st.session_state['scratch_bool']:
-            area_user_select = st.number_input('Select Area (sqft)', min_value = 0.0)
-            user_inputs['area'] = area_user_select
-        else:
-            area_user_select = st.number_input('Select Area (sqft)', min_value = 0.0, value = df.iloc[zip_index_int]['area'])
-            user_inputs['area'] = area_user_select
-
-        # Critical Flag
-        if st.session_state['scratch_bool']:
-            flags_user_select = st.number_input('Critical Flags', min_value = 0.0)
-            user_inputs['critical flag'] = flags_user_select
-        else:
-            flags_user_select = st.number_input('Critical Flags', min_value = 0.0, value = df.iloc[zip_index_int]['critical flag'])
-            user_inputs['critical flag'] = flags_user_select
-        
-        # Action
-        if st.session_state['scratch_bool']:
-            actions_user_select = st.number_input('Number of Actions', min_value = 0.0)
-            user_inputs['action'] = actions_user_select
-        else:
-            actions_user_select = st.number_input('Number of Actions', min_value = 0.0, value = df.iloc[zip_index_int]['action'])
-            user_inputs['action'] = actions_user_select
+            user_inputs['population'] = [pop_user_select]
 
         # Average Score
         st.markdown('Choose a health rating from 0 (best) to 40 (worst):')
@@ -116,6 +93,14 @@ if df is not None:
         else:
             health_rating_user_select = st.slider('A(0-13) B(14-27) C(28-40)', min_value=0, max_value=40, value=int(df.iloc[zip_index_int]['avg score']))
             user_inputs['avg score'] = health_rating_user_select
+
+        # Critical Flag
+        if st.session_state['scratch_bool']:
+            flags_user_select = st.number_input('Critical Flags', min_value = 0.0)
+            user_inputs['critical flag'] = flags_user_select
+        else:
+            flags_user_select = st.number_input('Critical Flags', min_value = 0.0, value = df.iloc[zip_index_int]['critical flag'])
+            user_inputs['critical flag'] = flags_user_select
         
         # Sidewalk Dimensions (SQFT)
         if st.session_state['scratch_bool']:
@@ -143,7 +128,7 @@ if df is not None:
                 sidewalk_seating_user_select = st.number_input('Number of Restaurants Approved for Sidewalk Seating', min_value = 0, value = df.iloc[zip_index_int]['approved for sidewalk seating'])
                 user_inputs['approved for sidewalk seating'] = sidewalk_seating_user_select
         with(col2):
-            # Approved for Sidewalk Seating
+            # Approved for Roadway Seating
             if st.session_state['scratch_bool']:
                 roadway_seating_user_select = st.number_input('Number of Restaurants Approved for Roadway Seating', min_value = 0)
                 user_inputs['approved for roadway seating'] = roadway_seating_user_select
@@ -167,6 +152,8 @@ if df is not None:
             num_restaurants_user_select = st.number_input('Number of Restaurants', min_value = 0, value = df.iloc[zip_index_int]['total_number_restaurants'])
             user_inputs['total_number_restaurants'] = num_restaurants_user_select
         
-        st.write(user_inputs)
+        num_rats = deploy_model(model,user_inputs)
+
+        st.write('From our model we can predict that {} rat citings will occur each year'.format(num_rats))
 
     
