bb9599a9c54ef731b8028c02950ffc47a21ed585
Matthew Shen <78379726+MatthewDShen@users.noreply.github.com>
2023-05-12 18:32:09 -0400

    some more viz stuff

--- rat-city.py
+++ rat-city.py
@@ -18,11 +18,21 @@ st.markdown('### On this page you can explore the data that we used to predict t
 df = pd.read_csv('data/processed_data/feature_data.csv')
 
 if df is not None:
-    # st.write(df)
+    st.markdown('### Original Datasets')
+
+    st.markdown('- [Restaurant Inspection by DOHMH](https://data.cityofnewyork.us/Health/DOHMH-New-York-City-Restaurant-Inspection-Results/43nn-pn8j)')
+    st.markdown('- [Open Restaurant Applications](https://data.cityofnewyork.us/Transportation/Open-Restaurant-Applications/pitm-atqc)')   
+    st.markdown('- [Rat Complaints through 311](https://data.cityofnewyork.us/City-Government/311-Call-Center-Inquiry/wewp-mm3p)')
+    st.markdown('- [Zipcode shapefile (for plotting)](https://data.cityofnewyork.us/Business/Zip-Code-Boundaries/i8iw-xf4u/data)')
+    
+
+    st.markdown('### Preprocessed Data')
+    st.write(df)
+
     st.markdown('#### Explore Features')
 
     # Select Feature
-    explore_feature_lst = ['zipcode', 'population', 'area', 'rat count', 'critical flag', 'action', 'avg score', 'sidewalk dimensions (area)', 'roadway dimensions (area)', 'approved for sidewalk seating', 'approved for roadway seating', 'qualify alcohol', 'total_number_restaurants']
+    explore_feature_lst = ['zipcode', 'rat count', 'population', 'avg score', 'critical flag', 'sidewalk dimensions (area)', 'roadway dimensions (area)', 'approved for sidewalk seating', 'approved for roadway seating', 'qualify alcohol', 'total_number_restaurants']
     explore_feature_str = st.selectbox('What feature would you like to explore?', explore_feature_lst)
 
     # Histogram
@@ -36,6 +46,10 @@ if df is not None:
     plt.title('New York City {}'.format(explore_feature_str))
     ax.axis('off')
     st.pyplot(fig)
+
+    st.markdown('##### Correlations with rat count')
+    corr_float = round(df['rat count'].corr(df[explore_feature_str]),3)
+    st.write('The correlation between rat count and {0} is {1}'.format(explore_feature_str, corr_float))
 
     # Store Dataframe
     st.session_state['df'] = df
